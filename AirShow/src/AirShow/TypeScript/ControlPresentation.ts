91b2b194ad98e7e2cfa50bf9c023a1d058bd4ca2
costinAndronache <andronache.costin@gmail.com>
2017-01-23 04:38:11 +0200

    ...

--- AirShow/src/AirShow/TypeScript/ControlPresentation.ts
+++ AirShow/src/AirShow/TypeScript/ControlPresentation.ts
@@ -75,7 +75,7 @@ class PointerCanvasController {
         }
 
         var pointerHandler = function (ev: PointerEvent) {
-            alert('touches moved ' + ev.offsetX + ", " + ev.offsetY);
+            redrawWithCoordinates(ev.offsetX, ev.offsetY);
         }
 
         var eventType: string;
